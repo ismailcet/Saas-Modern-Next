b562fbb37426eedc4c58cfcfb437276d678685c7
ismailcet <ismailcet3@gmail.com>
2023-05-09 00:35:17 +0300

    Some Changes are added in HomePage

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -2,15 +2,42 @@ import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
-  return (
-    <section  >
-       <div className='bgImage'></div>
-       <div className="form-content">
-        <h3 className="form-title">Coworking space that
-          will feel like home.</h3>
-        <p className="form-text">Whether you’re an established enterprise or a growing startup, discover spaces that inspire which will best your most impactful work.</p>
-        
 
+  const city:{id: number, name: String}[] =[
+    {
+      id:0,
+      name:"Brooklyn, New york"
+    },
+    {
+      id:1,
+      name:"Atlanta, Georgia"
+    },
+    {
+      id:2,
+      name:"Minneapolis, Minnesota"
+    },
+    {
+      id:3,
+      name:"Chicago, Illinois"
+    }
+  ]
+
+  return (
+    <section  className='mt-28'>
+       <div className='bgImage'></div>
+       <div className="bg-white w-1/2 py-12 px-[5rem]">
+        <h3 className="text-5xl font-bold leading-[1.57] text-main-color ">Coworking space that
+          will feel like home.</h3>
+        <p className="text-lg font-normal text-main-color mt-5 mb-10 leading-[2.48] max-w-[440px]">Whether you’re an established enterprise or a growing startup, discover spaces that inspire which will best your most impactful work.</p>
+        <div className='flex flex-col gap-5'>
+          <select name="country" id="country">
+            {city.map((c)=>(
+              <option key={c.id}>{c.name}</option>
+            ))}
+          </select>
+
+          <button>Subscribe</button>
+        </div>
        </div>
     </section>
   )
