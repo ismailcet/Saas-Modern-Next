49e7d04669409936efc3d5f298b7a8e27752c6b1
ismailcet <ismailcet3@gmail.com>
2023-05-10 17:08:55 +0300

    Header Section is finished and Customer Section is started to design

--- app/components/Customer/Customer.tsx
+++ app/components/Customer/Customer.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Customer = () => {
   return (
-    <div>Customer</div>
+    <section className='mt-[3.75rem] flex flex-col items-center mx-auto'>
+      <div>
+        <p className='text-second-color font-extrabold mb-3 text-center'>Meet Our Clients</p>
+        <h3>This guys are using our service and they're more than happier ever.</h3>
+      </div>
+      <div className='flex '>
+          <Image src="/images/clients/uber.png" alt="uber" width={100} height={21}/>
+          <Image src="/images/clients/google.png" alt="google" width={100} height={21} />
+          <Image src="/images/clients/paypal.png" alt="paypal" width={100} height={21} />
+          <Image src="/images/clients/microsoft.png" alt="microsoft" width={100} height={21} />
+          <Image src="/images/clients/dribbble.png" alt="dribbble" width={100} height={21} />
+      </div>
+    </section>
   )
 }
 
--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
-
+import {BiMap} from "react-icons/bi"
 const Header = () => {
 
   const city:{id: number, name: String}[] =[
@@ -23,20 +23,23 @@ const Header = () => {
   ]
 
   return (
-    <section  className='mt-28'>
+    <section  className='mt-28 mb-[11.25rem]'>
        <div className='bgImage'></div>
-       <div className="bg-white w-1/2 py-12 px-[5rem]">
-        <h3 className="text-5xl font-bold leading-[1.57] text-main-color ">Coworking space that
+       <div className="bg-white w-full md:w-1/2 py-12 px-[5rem]">
+        <h3 className="text-5xl md:text-2xl md:leading-[1.2] lg:text-5xl lg:leading-[1.57] font-bold text-main-color ">Coworking space that
           will feel like home.</h3>
-        <p className="text-lg font-normal text-main-color mt-5 mb-10 leading-[2.48] max-w-[440px]">Whether you’re an established enterprise or a growing startup, discover spaces that inspire which will best your most impactful work.</p>
-        <div className='flex flex-col gap-5'>
-          <select name="country" id="country">
-            {city.map((c)=>(
-              <option key={c.id}>{c.name}</option>
-            ))}
-          </select>
-
-          <button>Subscribe</button>
+        <p className="text-lg leading-[2.48]  font-normal text-main-color mt-5 mb-10 max-w-[440px]">Whether you’re an established enterprise or a growing startup, discover spaces that inspire which will best your most impactful work.</p>
+        <div className='flex flex-col'>
+          <div className='w-full relative'>
+            <BiMap className='absolute text-4xl font-normal text-disabled-color top-1/4 left-3'/>
+            <label htmlFor="country" className='absolute top-2 left-[4.4rem]'>Find Workspace</label>
+            <select name="country" id="country" className='min-h-[75px] border-second-color border-2 rounded-[5px] px-16 pt-4 font-bold'>
+              {city.map((c)=>(
+                <option key={c.id}>{c.name}</option>
+              ))}
+            </select>
+          </div>
+          <button className='w-full bg-second-color text-white mt-5 rounded-[5px] font-bold text-xl min-h-[60px] '>Subscribe</button>
         </div>
        </div>
     </section>
